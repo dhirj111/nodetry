4d05e2df59f55f5ab4de07dd8adeccbcafed1da6
dhirj111 <jdreamcreations@gmail.com>
2024-09-28 14:59:40 +0530

    added server

--- app1.js
+++ app1.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+const httpmodule1 = require('http');
+let server1 =httpmodule1.createServer(function(req1 , res1){
+console.log(req1)
+console.log("name printed")
+})
+
+server1.listen('4000')
