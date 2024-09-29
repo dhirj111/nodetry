d8d4737e9cb219d5f4c2976894b9823f40a8b3f9
dhirj111 <jdreamcreations@gmail.com>
2024-09-29 15:55:03 +0530

    on / url  genearetd a user input form containing form action that changes url to / messaage and then an if block that changes the url again to / witrh redirect  code = 302

--- app1.js
+++ app1.js
@@ -1,23 +1,25 @@
 const httpmodule1 = require("http");
-const { text } = require("stream/consumers");
 let server1 = httpmodule1.createServer(function (req1, res1) {
-
-  //req.url prints  which url we are performing request on , it print whatver is after localhost:4000 in current case
-  //while comparing it with /home/  ,'/home/' doesn.t work
-  if (req1.url == /home/) {
+  if (req1.url == "/") {
     res1.setHeader("content-type", "text/html");
-    res1.write("<h1>Welcome home</h1>");
-    res1.end();
-  } else if (req1.url == /about/) {
-    res1.setHeader("content-type", "text/html");
-    res1.write("<h1>Welcome to About Us page</h1>");
-    res1.end();
-  } else if (req1.url == /node/) {
-    res1.setHeader("content-type", "text/html");
-    res1.write("<h1>Welcome to my Node Js project</h1>");
+    res1.write("<html>");
+    res1.write("<head>message page</head>");
+    res1.write(
+      '<body><form action="/message" method ="POST"><input type="text" ><button type="submit">send</button></form></body>'
+    );
+    //remember strictly single   ' '  is required no ""
+    //form action="/message" automatically works on host and changes it to localhost:4000/message
+    res1.write("</html>");
     res1.end();
   }
-  process.exit();
+// after above line code thread reaches below and divert url as wrote
+  if(req1.url == "/message" && req1.method == "POST"){
+    res1.statusCode= 302; // redirct code
+    res1.setHeader('Location' , "/") // location is a syntax not variable 
+    res1.end()
+    //it again changes directory url location / instead of / message that we done by form action 
+  }
+
 });
 
 server1.listen("4000");
