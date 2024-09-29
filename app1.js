c33631bc7ba6d3d54cff2c24aee47efc7badbaa2
dhirj111 <jdreamcreations@gmail.com>
2024-09-29 13:39:54 +0530

    addeed responses for diffrent urls

--- app1.js
+++ app1.js
@@ -1,7 +1,20 @@
-const httpmodule1 = require('http');
-let server1 =httpmodule1.createServer(function(req1 , res1){
-console.log(req1)
-console.log("name printed")
-})
+const httpmodule1 = require("http");
+const { text } = require("stream/consumers");
+let server1 = httpmodule1.createServer(function (req1, res1) {
+  if (req1.url == /home/) {
+    res1.setHeader("content-type", "text/html");
+    res1.write("<h1>Welcome home</h1>");
+    res1.end();
+  } else if (req1.url == /about/) {
+    res1.setHeader("content-type", "text/html");
+    res1.write("<h1>Welcome to About Us page</h1>");
+    res1.end();
+  } else if (req1.url == /node/) {
+    res1.setHeader("content-type", "text/html");
+    res1.write("<h1>Welcome to my Node Js project</h1>");
+    res1.end();
+  }
+  process.exit();
+});
 
-server1.listen('4000')
+server1.listen("4000");
