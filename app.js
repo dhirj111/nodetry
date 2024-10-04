37cb968b3e990a0411e32560220e73a6c9d8fd1a
dhirj111 <jdreamcreations@gmail.com>
2024-10-04 14:03:27 +0530

    imported express depenedency stored it in variable app1 and created two middle ware with explanatation in comment

---
+++ app.js
@@ -0,0 +1,31 @@
+const httpmodule1 = require("http");
+const express1 = require('express')
+const app1 = express1();
+//express1 returns a function that we called above and stored in app1.js
+//now we can perform diffrent operations on app1
+//it does also behave as listner
+
+//express js is all about middlware
+//middleware means excetuable functionalities a colections of request reponse
+
+//middleware is created by .use on app1
+//.use accepts requesthandler functions or we can declare anonymous functions in .use brackets  too
+//but it should necessarily consist three parameters request response and next(a functions that 
+//we invoke to jump to next middleware)
+
+app1.use((req1 , res1 , next1)=>{
+console.log("in the middleware ")
+next1()
+//jumps to next middleware 
+})
+
+app1.use((req1 , res1 , next1)=>{
+  console.log("in the another middleware2 ")
+  next1()
+  //jumps to next middleware 
+  })
+let server1 = httpmodule1.createServer(app1);
+//we invoked 
+// here routes no needed to be invoke as its listner  and works writte way
+
+server1.listen("4000");
--- app1.js
+++
@@ -1,9 +0,0 @@
-const httpmodule1 = require("http");
-const routes1 = require('./routes.js');
-const requestHandler = require("./routes.js");
-// we imported  routes.js  by global modules and stored that in routes1
-// added file system
-let server1 = httpmodule1.createServer(routes1);
-// here routes no needed to be invoke as its listner  and works writte way
-
-server1.listen("4000");
