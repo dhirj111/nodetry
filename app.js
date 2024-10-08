9f24d79da772f676872c2ab517ba48861fad49af
dhirj111 <jdreamcreations@gmail.com>
2024-10-08 11:32:59 +0530

    created an input form as reponse of /add-product route for description and size , >> installed and imported body parser ,passed it to app1.js >> handled /product  by printing parsed object description and size

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 // const httpmodule1 = require("http");
 const express1 = require("express");
 const app1 = express1();
-//express1 returns a function that we called above and stored in app1.js
-//now we can perform diffrent operations on app1
-//it does also behave as listner
+const bodyparser1 = require("body-parser");
 
-//express js is all about middlware
-//middleware means excetuable functionalities a colections of request reponse
+app1.use(bodyparser1.urlencoded({ extended: true }));
 
-//middleware is created by .use on app1
-//.use accepts requesthandler functions or we can declare anonymous functions in .use brackets  too
-//but it should necessarily consist three parameters request response and next(a functions that
-//we invoke to jump to next middleware)
+//must have to use l4 and l16 to use req1.body inside middlewares
 
-app1.use('/add-product', (req1, res1, next1) => {
+app1.use("/add-product", (req1, res1, next1) => {
   console.log("in the middleware ");
-  res1.send("<h1> the add payment page <h1>");
-  next1();
-  //jumps to next middleware
+  res1.send(
+    '<form action = "/product" method ="POST">description<input type = "text" name= "title">size <input type = "number" name= "size"><button type="submit">Add Product</button></form>'
+  );
+//this will send reponse as object containg key as title we wrote and value as user inputted values but unparsed
 });
 
-app1.use('/',(req1, res1, next1) => {
-  //remember '/' is route which every route contains like  /about /addproduct
-  //so if u will use before other custom routes it will execute / and then jump to next()[if invoked]
-  //so it will execute for every route apart from above custome route middleware
 
-  console.log("in the another middleware2 ");
-  
-  //jumps to next middleware
-  //removed next1() as there is no middleware to jump and nor required
-
-  res1.send("<h1> Hello from express </h1>");
-  //response.send sends reponse as coder define in bracket
+//below middleware just works for post request on mentionede route/address
+app1.post("/product", (req1, res1, next1) => {
+  console.log("in body printer");
+  console.log(req1.body.title);
+  console.log(req1.body.size);
+  //as object is parsed it behaves like object 
+  //while creating html code when we was writing titles they are actually keys of object so
+  //we can access value in form of key value pairs
+  res1.redirect("/");
+  //.redirect and then route address
+});
+app1.use("/", (req1, res1, next1) => {
+  console.log("inside default / ");
+  res1.send("<h1> in / middleware</h1>");
 });
 // let server1 = httpmodule1.createServer(app1);
 // //we invoked
@@ -42,4 +40,7 @@ app1.use('/',(req1, res1, next1) => {
 //and then listening to port that can be done as below  ,no need to import http too
 //you can read express git docu.. there code is written of working
 //https://github.com/expressjs/express/blob/master/lib/application.js  line 609-617
-app1.listen(4000);
+app1.listen(4000, () => {
+  console.log("server is running on port 4000");
+});
+//you can also write anon function to .listen of express
