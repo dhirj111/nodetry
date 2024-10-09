7559709d58277ee51a63d14be1dabbd648fb3f2f
dhirj111 <jdreamcreations@gmail.com>
2024-10-09 16:44:47 +0530

    added a default middleware to handle all not available paths

--- app.js
+++ app.js
@@ -2,44 +2,19 @@
 const express1 = require("express");
 const app1 = express1();
 const bodyparser1 = require("body-parser");
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
 
+//we exported files our next  step will be is to pass variable names(adminroutes , shoproutes) 
+//as a requestlistner to app1.methods lik app11.use [done in line 12 and 13 in this commit]
 app1.use(bodyparser1.urlencoded({ extended: true }));
-
 //must have to use l4 and l16 to use req1.body inside middlewares
+app1.use(adminRoutes);
+app1.use(shopRoutes);
+app1.use((req1 ,res1 , next1)=>{
 
-app1.use("/add-product", (req1, res1, next1) => {
-  console.log("in the middleware ");
-  res1.send(
-    '<form action = "/product" method ="POST">description<input type = "text" name= "title">size <input type = "number" name= "size"><button type="submit">Add Product</button></form>'
-  );
-//this will send reponse as object containg key as title we wrote and value as user inputted values but unparsed
-});
-
-
-//below middleware just works for post request on mentionede route/address
-app1.post("/product", (req1, res1, next1) => {
-  console.log("in body printer");
-  console.log(req1.body.title);
-  console.log(req1.body.size);
-  //as object is parsed it behaves like object 
-  //while creating html code when we was writing titles they are actually keys of object so
-  //we can access value in form of key value pairs
-  res1.redirect("/");
-  //.redirect and then route address
-});
-app1.use("/", (req1, res1, next1) => {
-  console.log("inside default / ");
-  res1.send("<h1> in / middleware</h1>");
-});
-// let server1 = httpmodule1.createServer(app1);
-// //we invoked
-// // here routes no needed to be invoke as its listner  and works writte way
-// server1.listen("4000");
-//
-//instead of line 30 , 33 and line 1 to create a server having a  requesthandle(app1 in our case)
-//and then listening to port that can be done as below  ,no need to import http too
-//you can read express git docu.. there code is written of working
-//https://github.com/expressjs/express/blob/master/lib/application.js  line 609-617
+res1.status(404).send('<h1>error 404 </h1><h4>just implmented default middleware with res methods = status(404).send</h4>')
+})
 app1.listen(4000, () => {
   console.log("server is running on port 4000");
 });
