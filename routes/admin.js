65e47872831cda29d79d8e2a0c44f2326a0644c0
dhirj111 <jdreamcreations@gmail.com>
2024-10-11 14:16:10 +0530

    installed node-localstorage which created new folder containing username through line 11 of shop.js , rest in code

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,25 @@
-const express1 = require('express');
+const express1 = require("express");
 //imported expres and saved in a constant
 
 const router = express1.Router();
+const bodyparser1 = require("body-parser");
+router.use(bodyparser1.urlencoded({ extended: true }));
 // stored Router method on express1 constant in a new constant
-// now we can code the middlewares but we have to use methods on 
+// now we can code the middlewares but we have to use methods on
 // router instead of app as router.use('/', { ..middleware})
 
 router.use("/add-product", (req1, res1, next1) => {
   console.log("in the middleware ");
   res1.send(
-    '<form action = "/admin/product" method ="POST">description<input type = "text" name= "title">size <input type = "number" name= "size"><button type="submit">Add Product</button></form>'
+    '<form action = "/product" method ="POST">description<input type = "text" name= "title">size <input type = "number" name= "size"><button type="submit">Add Product</button></form>'
   );
 });
-
+router.use("/login", (req1, res1, next1) => {
+  console.log("in username ask form");
+  res1.send(
+    '<form action = "/" method ="POST">type username <input type = "text" name= "username"><button type="submit">save username</button></form>'
+  );
+});
 
 //below middleware just works for post request on mentionede route/address
 router.use("/product", (req1, res1, next1) => {
@@ -23,6 +30,5 @@ router.use("/product", (req1, res1, next1) => {
   //.redirect and then route address
 });
 
-
-module.exports = router
+module.exports = router;
 //we exported above defined router constant in module so that we can import and use this module in app.js
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,55 @@
-const express1 = require('express');
+const express1 = require("express");
 //imported expres and saved in a constant
 
+const LocalStorage = require("node-localstorage").LocalStorage;
+//installed and used localstorae here and in  line 11
+//imported node-ls with .ls method in ls variable
+const filesystem1 = require("fs");
 const router = express1.Router();
+const bodyparser1 = require("body-parser");
+router.use(bodyparser1.urlencoded({ extended: true }));
+const localStorage = new LocalStorage("./scratch");
 // stored Router method on express1 constant in a new constant
-// now we can code the middlewares but we have to use methods on 
+// now we can code the middlewares but we have to use methods on
 // router instead of app as router.use('/', { ..middleware})
 
+router.use("/", (req1, res1) => {
+  // Store the username if submitted
+  let username = localStorage.getItem("username");
+  
+  // Store the username if submitted
+  if (req1.body.username) {
+    username = req1.body.username;
+    localStorage.setItem("username", username);
+  }
 
-router.use("/", (req1, res1, next1) => {
-  console.log("inside default / ");
-  res1.send("<h1> in / middleware</h1>");
+  console.log("Current username:", username);
+
+
+  const content = filesystem1.readFileSync('message1.txt', 'utf8');
+  // console.log("File content:", content);
+
+  // If a message is submitted, append it to the file
+  if (req1.body.msg) {
+    const newMessage = `${username}: ${req1.body.msg}\n`;
+    filesystem1.appendFileSync("message1.txt", newMessage);
+    // console.log("Appended message:", newMessage);
+  }
+
+  // Reload the file content to display updated messages
+  const updatedContent = filesystem1.readFileSync('message1.txt', 'utf8');
+
+  // Send response to display previous messages and ask for a new message
+  res1.send(`
+    <p>Previous messages:</p>
+    <pre>${updatedContent}</pre>
+    <form action="/" method="POST">
+      <label for="msg">Enter your message:</label>
+      <input type="text" name="msg" required>
+      <button type="submit">Send Message</button>
+    </form>
+  `);
 });
 
-
-
-
-module.exports = router
+module.exports = router;
 //we exported above defined router constant in module so that we can import and use this module in app.js
